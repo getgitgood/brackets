98c8f2e1a08a4af62cf377dae0e724a7c8aa2a02
Kotec <vivalastelmah@gmail.com>
2023-02-28 02:05:06 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const PAIRING_BRACKETS = {};
+  let bracketsStack = [];
+
+  if (str.length % 2) {
+    return false;
+  }
+
+  bracketsConfig.forEach(item => {
+    PAIRING_BRACKETS[item[1]] = item[0]
+  })
+
+  for (let i = 0; i < str.length; i++) {
+    let upperElement = bracketsStack[bracketsStack.length - 1];
+    if (bracketsStack.length && PAIRING_BRACKETS[str[i]] === upperElement) {
+      bracketsStack.pop()
+    } else {
+      bracketsStack.push(str[i])
+    }
+  }
+
+  return bracketsStack.length === 0
 }
+
